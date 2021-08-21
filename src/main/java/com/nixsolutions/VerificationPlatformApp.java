8e7400d75843057c08462996535179783ef17f7e
Влад Шахно <v.shahno@outlook.com>
2021-08-21 16:38:14 +0300

    Created and configured application.properties and main class VerificationPlatformApp

---
+++ src/main/java/com/nixsolutions/VerificationPlatformApp.java
@@ -0,0 +1,13 @@
+package com.nixsolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerificationPlatformApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VerificationPlatformApp.class, args);
+
+    }
+}
