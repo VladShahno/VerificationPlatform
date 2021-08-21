6f3d94da7ec0e7c5e1b71fc75b20196a97507fe0
Влад Шахно <v.shahno@outlook.com>
2021-08-21 20:18:50 +0300

    Created CompanyServiceImpl, add boolean method existsByName in LeadRepository

---
+++ src/main/java/com/nixsolutions/persistance/service/impl/CompanyServiceImpl.java
@@ -0,0 +1,60 @@
+package com.nixsolutions.persistance.service.impl;
+
+import com.nixsolutions.persistance.entity.Company;
+import com.nixsolutions.persistance.repository.CompanyRepository;
+import com.nixsolutions.persistance.service.CompanyService;
+
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+public class CompanyServiceImpl implements CompanyService {
+
+    private final CompanyRepository companyRepository;
+
+    public CompanyServiceImpl(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+    @Override
+    public void create(Company company) {
+        if (!companyRepository.existsByName(company.getCompanyName())) {
+            companyRepository.save(company);
+        } else
+            throw new RuntimeException("Company with name - " + company.getCompanyName() + " already exists!");
+    }
+
+    @Override
+    public void update(Company company) {
+        if (companyRepository.existsById(company.getId())) {
+            if (companyRepository.existsByName(company.getCompanyName())) {
+                companyRepository.save(company);
+            } else
+                throw new RuntimeException("Company with name - " + company.getCompanyName() + " does not exist!");
+        } else
+            throw new RuntimeException("Company with id - " + company.getId() + " does not exist!");
+    }
+
+    @Override
+    public void delete(Integer id) {
+        if (companyRepository.existsById(id)) {
+            companyRepository.deleteById(id);
+        } else
+            throw new RuntimeException("Company with id - " + id + " does not exist!");
+    }
+
+    @Override
+    public Company find(Integer id) {
+        if (companyRepository.existsById(id)) {
+            return companyRepository.getById(id);
+        } else
+            throw new RuntimeException("Company with id - " + id + " does not exist!");
+    }
+
+    @Override
+    public Page<Company> find(Map<String, String> params) {
+        return null;
+    }
+}
