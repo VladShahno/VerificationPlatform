01636f378340be3ee1173a0a9299fc1526f1be2b
Влад Шахно <v.shahno@outlook.com>
2021-09-16 08:36:05 +0300

    refactor repositories classes

--- src/main/java/com/nixsolutions/platform/persistence/repository/LeadRepository.java
+++ src/main/java/com/nixsolutions/platform/persistence/repository/LeadRepository.java
@@ -1,6 +1,7 @@
 package com.nixsolutions.platform.persistence.repository;
 
 import com.nixsolutions.platform.persistence.entity.Lead;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
